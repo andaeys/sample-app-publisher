127b2072cb484f8ee92f9ea0629eefc93d218b9b
andaeys <ekayudha.sulistianda@quipper.com>
2020-12-10 17:03:08 +0700

    cleaning up the constans

--- src/main/java/app/ApplicationConfig.kt
+++ src/main/java/app/ApplicationConfig.kt
@@ -8,12 +8,12 @@ class ApplicationConfig {
          * `null` or blank, the application will log a warning. Suggested
          * format is "MyCompany-Application/1.0".
          */
-        val APPLICATION_NAME = "Quipper"
+        const val APPLICATION_NAME = "Quipper"
 
         /**
          * Specify the package name of the app.
          */
-        val PACKAGE_NAME = "com.quipper.school.assignment"
+        const val PACKAGE_NAME = "com.quipper.school.assignment"
 
         /**
          * Authentication.
@@ -28,7 +28,7 @@ class ApplicationConfig {
          * account email and add your key.p12 file to the resources directory.
          *
          */
-        val SERVICE_ACCOUNT_EMAIL = "learn-android@api-4918360869960997146-390636.iam.gserviceaccount.com"
+        const val SERVICE_ACCOUNT_EMAIL = "your-app-name@api-xxxxxxx.iam.gserviceaccount.com"
 
 
         /**
@@ -39,8 +39,8 @@ class ApplicationConfig {
          * samples.
          *
          */
-        val APK_FILE_PATH =
-            "/Users/quipperindonesia/Workspace/android-play-publisher-api/v3/test-publish/src/main/resources/base_35.apk"
+        const val APK_FILE_PATH =
+            "~/../../base_36_high.apk"
     }
 
     private fun ApplicationConfig() {
