04061316fc6854696cab62f273d6d182cac49961
andaeys <ekayudha.sulistianda@quipper.com>
2020-12-13 21:43:50 +0700

    test jar 2

--- src/main/java/app/ArgsTest.kt
+++ src/main/java/app/ArgsTest.kt
@@ -4,9 +4,10 @@ object ArgsTest {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        val p1 = args[0]
-        val p2 = args[1]
-
-        println("p1: $p1 | p2: $p2")
+//        val p1 = args[0]
+//        val p2 = args[1]
+//
+//        println("p1: $p1 | p2: $p2")
+        println("Hello")
     }
 }
