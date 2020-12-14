69be6fa1b09f968884023adf841318216b3fb46c
andaeys <ekayudha.sulistianda@quipper.com>
2020-12-14 15:30:45 +0700

    Implement upload aab from executing jar file

--- src/main/java/app/Settings.kt
+++ src/main/java/app/Settings.kt
@@ -3,5 +3,8 @@ package app
 @kotlinx.serialization.Serializable
 data class Settings(
     val serviceAccountEmail: String,
-    val track: String
+    val track: String,
+    val releaseName: String,
+    val releaseNotes: String,
+    val releasePriority: Int
 )
